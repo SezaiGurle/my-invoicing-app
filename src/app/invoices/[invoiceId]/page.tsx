71a3816c254557321dd76fdfe0d08f314e3b409f
SezaiGurle <sezaigurle35@gmail.com>
2024-12-31 22:28:10 +0300

    Update page.tsx
    Update page.tsx  for vercel

--- src/app/invoices/[invoiceId]/page.tsx
+++ src/app/invoices/[invoiceId]/page.tsx
@@ -1,42 +1,62 @@
-import { auth } from "@clerk/nextjs/server";
-import { eq } from "drizzle-orm";
 import { notFound } from "next/navigation";
+import { auth } from "@clerk/nextjs/server";
 
 import { db } from "@/db";
 import { Customers, Invoices } from "@/db/schema";
+import { eq, and, isNull } from "drizzle-orm";
+
 import Invoice from "./Invoice";
 
-export default async function InvoicePage({
-  params,
-}: { params: { invoiceId: string } }) {
-  const { userId } = await auth();
+export default async function InvoicePage({ params }: { params: { invoiceId: string } }) {
+    const { userId, orgId } = await auth();
 
-  if (!userId) return;
+    if (!userId) return notFound();
 
-  const invoiceId = Number.parseInt(params.invoiceId);
+    const { invoiceId: rawInvoiceId } = await params; // Ensure `params` is awaited
+    const invoiceId = Number.parseInt(rawInvoiceId);
 
-  if (Number.isNaN(invoiceId)) {
-    throw new Error("Invalid Invoice ID");
-  }
+    if (isNaN(invoiceId)) {
+        throw new Error("Invalid Invoice ID");
+    }
+    let result;
 
-  // Veritabanı sorgusuyla bir sonuç alıyoruz
-  const [result]: Array<{
-    invoices: typeof Invoices.$inferSelect;
-    customers: typeof Customers.$inferSelect;
-  }> = await db
-    .select()
-    .from(Invoices)
-    .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
-    .limit(1);
+    if ( orgId ) {
+        [result] = await db
+            .select()
+            .from(Invoices)
+            .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+            .where(
+                and(
+                    eq(Invoices.id, invoiceId),
+                    eq(Invoices.organizationId, orgId)
+                )
+            )
+            .limit(1);
+    }else {
+        [result] = await db
+            .select()
+            .from(Invoices)
+            .innerJoin(Customers, eq(Invoices.customerId, Customers.id))
+            .where(
+                and(
+                    eq(Invoices.id, invoiceId),
+                    eq(Invoices.userId, userId),
+                    isNull(Invoices.organizationId)
+                )
+            )
+            .limit(1);
+    }
 
-  if (!result) {
-    return notFound();
-  }
 
-  const invoice = {
-    ...result.invoices,
-    customer: result.customers,
-  };
+    if (!result) {
+        notFound();
+    }
 
-  return <Invoice invoice={invoice} />;
+    const invoices = {
+        ...result.invoices,
+        customer: result.customers
+    }
+
+    return <Invoice invoice={invoices} />;
 }
+
